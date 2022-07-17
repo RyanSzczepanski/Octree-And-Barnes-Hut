487e42faba4f16badc9693c460532fa1efbc7cab
RyanSzczepanski <Slyry11@gmail.com>
2022-07-16 23:07:50 -0400

    Added Node Building Structure

--- Assets/Octree/CreateTree.cs
+++ Assets/Octree/CreateTree.cs
@@ -7,10 +7,45 @@ public class CreateTree : MonoBehaviour
 {
     public int depth;
     public NativeList<Node<Vector3>> nodes;
-    public NativeArray<Node<Vector3>> nodes;
 
     private void Start()
     {
-        nodes = new NativeArray<Node<Vector3>>(1 + (depth.);
+        nodes = new NativeList<Node<Vector3>>(0, Allocator.Persistent);
+        
+
+
+        int nodeCount = 1;
+
+        for (int currentDepth = 0; currentDepth < depth; currentDepth++)
+        {
+            for (int child = 0; child < Mathf.Pow(8, currentDepth + 1); child++)
+            {
+                nodeCount++;
+            }
+        }
+
+        Debug.Log(nodeCount);
+
+        NativeList<Node<Vector3>> newNodes = new NativeList<Node<Vector3>>(nodeCount, Allocator.Persistent);
+        nodes.Add(Node<Vector3>.CreateNewNode(new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)), -1, 0));
+        
+        for (int i = 0; i < depth; i++)
+        {
+            int nodeLength = nodes.Length;
+            for (int j = 0; j < nodeLength; j++)
+            {
+                if (nodes[j].endNode)
+                {
+                    Node<Vector3> currentNode = nodes[j];
+                    for (int k = 0; k < 8; k++)
+                    {
+                        nodes.Add(currentNode.PopulateChild(k, new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)), nodes.Length));
+                    }
+                    nodes[j] = currentNode;
+                }
+            }
+        }
+
+        Debug.Log(nodes.Length);
     }
 }
--- Assets/Octree/Node.cs
+++ Assets/Octree/Node.cs
@@ -8,7 +8,8 @@ public struct Node<Data>
     public Data data;
     public int parentNodeIndex;
     public int index;
-    public NativeArray<int> childrenIndexes;
+    public bool endNode;
+    public NodeChildren nodeChildren;
 
 
     public int GetMasterNodeIndex(NativeArray<Node<Data>> nodes)
@@ -20,8 +21,78 @@ public struct Node<Data>
         return -1;
     }
 
-    public void DestroyNode()
+    public static Node<Data> CreateNewNode(Data data, int parentNodeIndex, int index)
     {
-        childrenIndexes.Dispose();
+        return new Node<Data>() {
+            data = data,
+            parentNodeIndex = parentNodeIndex,
+            index = index,
+            endNode = true,
+            nodeChildren = new NodeChildren() {
+                child0 = -1,
+                child1 = -1,
+                child2 = -1,
+                child3 = -1,
+                child4 = -1,
+                child5 = -1,
+                child6 = -1,
+                child7 = -1,
+            },
+        };
+    }
+
+    public Node<Data> PopulateChild(int siblingIndex, Data data, int index)
+    {
+        endNode = false;
+        if(nodeChildren.GetChildIndex(siblingIndex) != -1)
+        {
+            Debug.Log($"Child {siblingIndex} of Node Index {index} is already populated");
+        }
+        nodeChildren.SetChildIndex(siblingIndex, index);
+        return CreateNewNode(data, this.index, index);
+    }
+}
+
+public struct NodeChildren
+{
+    public int child0;
+    public int child1;
+    public int child2;
+    public int child3;
+    public int child4;
+    public int child5;
+    public int child6;
+    public int child7;
+
+    public int GetChildIndex(int child)
+    {
+        switch (child)
+        {
+            case 0: return child0;
+            case 1: return child1;
+            case 2: return child2;
+            case 3: return child3;
+            case 4: return child4;
+            case 5: return child5;
+            case 6: return child6;
+            case 7: return child7;
+            default: return -1;
+        }
+    }
+
+    public int SetChildIndex(int child, int value)
+    {
+        switch (child)
+        {
+            case 0: return child0;
+            case 1: return child1;
+            case 2: return child2;
+            case 3: return child3;
+            case 4: return child4;
+            case 5: return child5;
+            case 6: return child6;
+            case 7: return child7;
+            default: return -1;
+        }
     }
 }
